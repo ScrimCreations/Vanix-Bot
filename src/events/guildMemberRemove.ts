e0f6a79c1a2f5b500e704994e4df95dd0ee82eaf
Mini <80361877+Mini51@users.noreply.github.com>
2022-11-29 00:11:50 -0500

    added guild remove update

---
+++ src/events/guildMemberRemove.ts
@@ -0,0 +1,13 @@
+import { Client, GuildMember, ActivityType } from "discord.js";
+
+export = {
+  name: "guildMemberRemove",
+  once: false,
+
+  async execute(member: GuildMember, client: Client) {
+    // Update the activity to the amount of members in the server when a member joins
+    client.user?.setActivity(`over ${client.guilds.cache.first()?.memberCount} members`, {
+      type: ActivityType.Watching,
+    });
+  },
+};
